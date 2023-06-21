078d6f575b1cc0f17faec15ed0c687013b1e7c1c
Himanshu <hemi8085@gmail.com>
2023-06-21 17:09:18 +0530

    developed showing game draw.extended game functionality

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,37 +1,39 @@
 
-import { useState } from 'react';
-import Square from './Square'
-const Board = ({squares, handleSquareClick}) => {
 
- const renderSquare = position => {
-  return (
-    <Square 
-    value = {squares[position]} 
-    onClick = {() => handleSquareClick(position)}
-    />
-  );
- };
+import Square from './Square';
+
+const Board = ({ squares, handleSquareClick, winningSquares }) => {
+  const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
 
     return (
-    <div className="board">
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
-        </div>
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
+      />
     );
+  };
+
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 };
+
 export default Board;
