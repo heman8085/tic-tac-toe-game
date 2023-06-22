57f3190bec36ee624b39f831fe7ec7f4fc58ab55
Himanshu <hemi8085@gmail.com>
2023-06-22 18:09:02 +0530

    included roboto font

--- src/winner.js
+++ src/winner.js
@@ -1,4 +1,5 @@
-export function calculateWinner(squares) {
+
+  export function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -12,8 +13,15 @@ export function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+        return {
+          winner: squares[a],
+          winningSquares: lines[i],
+        };
       }
     }
-    return null;
+  
+    return {
+      winner: null,
+      winningSquares: [],
+    };
   }
